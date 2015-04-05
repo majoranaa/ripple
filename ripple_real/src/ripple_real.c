2f8889719f6a7fafdcdb7e37fcced6cdb3a28d2c
Kai Yang <kai.yang412@gmail.com>
2015-04-05 03:23:31 -0700

    beginning dsp

--- ripple_real/src/ripple_real.c
+++ ripple_real/src/ripple_real.c
@@ -6,12 +6,16 @@
 
 #include <pebble.h>
 
+// 25 samples per second
 //#define NUM_SAMPLES 25
-#define ACCEL_STEP_MS 50
+#define ACCEL_STEP_MS 40
 
-#define MAKE_NEW_GESTURE 0
-#define NEW_GESTURE 1
-#define GESTURE 2
+#define KEY_MAKE_NEW_GESTURE 0
+#define KEY_NEW_GESTURE_ID 1
+#define KEY_NEW_GESTURE_DATA 2
+#define KEY_GESTURE 3
+#define KEY_OLD_GESTURE_ID 4
+#define KEY_OLD_GESTURE_DATA 5
 
 #define MAX_REF_SIZE 20 // this is the max number of samples that can be in a reference
 #define MAX_GESTURES 5
@@ -23,17 +27,27 @@ typedef struct {
   int16_t z;
 } DataVec;
 
+// window and layers
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_output_layer;
+
+// resources
 static GFont s_time_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
 // buffer for accel data
 static DataVec accel_buff[BUFF_SIZE];
-static DataVec *accel_head; // head of buffer
+static int head; // head of buffer
+static int start_proc; // begin processing
+static int make_gesture;
+static const float alpha = 0.1;
+static const float still_thresh = 1e5;
+static const int count_thresh = 4;
 
 // array of recorded gestures
+static DataVec temp_ges[MAX_REF_SIZE];
 static DataVec gestures[MAX_GESTURES][MAX_REF_SIZE];
 
 static void update_time() {
@@ -84,32 +98,85 @@ static void update_time() {
 
 static void timer_callback(void *data) {
   // Long lived buffer
-  // static char s_buffer[128];
-
+  static char s_buffer[128];
+  static float x_mavg;
+  static float y_mavg;
+  static float z_mavg;
+  static float x_diff;
+  static float y_diff;
+  static float z_diff;
+  static float still;
+  static int count = 0;
+  static int find_ref = 0;
+  
   AccelData accel = (AccelData) { .x = 0, .y = 0, .z = 0 };
   accel_service_peek(&accel);
 
   // Compose string of all data
-  // snprintf(s_buffer, sizeof(s_buffer), 
-  // "X: %d\nY: %d\nZ: %d\n",
-  // accel.x, accel.y, accel.z
-  // );
+  snprintf(s_buffer, sizeof(s_buffer), 
+	   "X: %d Y: %d Z: %d",
+	   accel.x, accel.y, accel.z);
   
   //Show the data
-  // text_layer_set_text(s_output_layer, s_buffer);
+  text_layer_set_text(s_output_layer, s_buffer);
 
   if (!accel.did_vibrate) {
-    
+    accel_buff[head].x = accel.x;
+    accel_buff[head].y = accel.y;
+    accel_buff[head].z = accel.z;
+    head = (head+1)%BUFF_SIZE;
+  }
+  if (head >= MAX_REF_SIZE) {
+    start_proc = 1;
+    APP_LOG(APP_LOG_LEVEL_INFO, "Starting processing");
+    x_mavg = accel_buff[head].x;
+    y_mavg = accel_buff[head].y;
+    z_mavg = accel_buff[head].z;
+    x_diff = 0;
+    y_diff = 0;
+    z_diff = 0;
+  }
+  if (start_proc) {
+    // Do dsp here
+    x_mavg = x_mavg + alpha*((float)accel.x - x_mavg);
+    y_mavg = y_mavg + alpha*((float)accel.y - y_mavg);
+    z_mavg = z_mavg + alpha*((float)accel.z - z_mavg);
+    x_diff = (float)accel.x - x_mavg;
+    y_diff = (float)accel.y - y_mavg;
+    z_diff = (float)accel.z - z_mavg;
+    still = x_diff*x_diff + y_diff*y_diff + z_diff*z_diff;
+    if (make_gesture) {
+      if (!find_ref) {
+	if (still < still_thresh) {
+	  count++;
+	  if (count >= count_thresh) {
+	    count = 0;
+	    find_ref = 1;
+	  }
+	}
+      } else {
+	
+      }
+    }
   }
   app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
 }
 
 static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+
   // Create GBitmap, then set to created BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+
+  // Create output TextLayer
+  s_output_layer = text_layer_create(GRect(5, 0, 139, 30));
+  text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(s_output_layer, "No data yet.");
+  text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
+  layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
 
   // Put time
   s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
@@ -121,23 +188,6 @@ static void main_window_load(Window *window) {
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-
-  /*
-    Layer *window_layer = window_get_root_layer(window);
-    GRect window_bounds = layer_get_bounds(window_layer);
-
-    // Create output TextLayer
-    s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, window_bounds.size.h));
-    text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-    text_layer_set_text(s_output_layer, "No data yet.");
-    text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
-    layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
-
-    s_output_layer2 = text_layer_create(GRect(5, 75, window_bounds.size.w - 10, window_bounds.size.h - 75));
-    text_layer_set_font(s_output_layer2, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-    text_layer_set_text(s_output_layer2, "Nothing going on");
-    text_layer_set_overflow_mode(s_output_layer2, GTextOverflowModeWordWrap);
-    layer_add_child(window_layer, text_layer_get_layer(s_output_layer2));*/
 }
 
 static void main_window_unload(Window *window) {
@@ -170,7 +220,36 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message received!");
+  APP_LOG(APP_LOG_LEVEL_INFO, "Message received!");
+
+  Tuple *t = dict_read_first(iterator);
+
+  // For all items
+  while(t != NULL) {
+    // Which key was received?
+    switch(t->key) {
+    case KEY_MAKE_NEW_GESTURE:
+      make_gesture = 1;
+      break;
+    case KEY_NEW_GESTURE_ID:
+      break;
+    case KEY_NEW_GESTURE_DATA:
+      break;
+    case KEY_GESTURE:
+      break;
+    case KEY_OLD_GESTURE_DATA:
+      break;
+    case KEY_OLD_GESTURE_ID:
+      break;
+    default:
+      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+      break;
+    }
+
+    // Look for next item
+    t = dict_read_next(iterator);
+  }
+
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
@@ -201,7 +280,9 @@ static void init() {
   // accel_data_service_subscribe(NUM_SAMPLES, data_handler); // **** this is batches
   accel_data_service_subscribe(0,NULL); // **** this is real time
   app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);
-  accel_head = accel_buff; // begin head at beginning of buffer
+  head = 0; // begin head at beginning of buffer
+  start_proc = 0;
+  make_gesture = 0;
 
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
